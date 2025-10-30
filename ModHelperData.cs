1e6dc3fb0e65a9b87827b79bfabd6e12da95b177
liamw <liamwolfguy@gmail.com>
2025-10-29 22:58:14 -0400

    Update description and icon

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,13 +2,13 @@ namespace Halloween2025;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "50.0";
+    public const string WorksOnVersion = "51.0";
     public const string Version = "1.0.0";
     public const string Name = "Halloween 2025";
 
-    public const string Description = "Halloween yippee!!!";
+    public const string Description = "Adds a ghost monkey tower and a plague doctor hero to the game";
 
-    public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "Halloween2025"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "DarkTerraYT";
+    public const string RepoName = "Halloween2025";
     public const string Author = "LerpMcGerk";
 }
